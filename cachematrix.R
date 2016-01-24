7c68001dc649670c87a4d2c74a26d36c79beda8c
rlangf <roman.langfeld@gmx.de>
2016-01-24 16:04:39 +0100

    Implemented the two functions for lexical scoping assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,28 +3,28 @@
 
 ## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(matrix = matrix()) {
+
+    ## initialize the inverse matrix to NULL
+	inverseMatrix <- NULL
 		
-		## initialize the inverse matrix to NULL
-		inverseMatrix <- NULL
+	## set the matrix
+	## function parameter newMatrix is assigned to variable matrix which is 
+    ## defined outside this setter function, thus the <<- operator is used
+	set <- function(newMatrix) {
+	    matrix <<- newMatrix
+	    inverseMatrix <<- NULL		## assuming the new matrix is different from the existing matrix, the inverse matrix is re-initialized to NULL
+	}
+	
+	## get the matrix
+	get <- function() matrix
 		
-		## set the matrix
-		## function parameter newMatrix is assigned to variable matrix which is 
-        ## defined outside this setter function, thus the <<- operator is used
-		set <- function(newMatrix) {
-				matrix <<- newMatrix
-				inverseMatrix <<- NULL		## assuming the new matrix is different from the existing matrix, the inverse matrix is re-initialized to NULL
-		}
-		
-		## get the matrix
-		get <- function() matrix
-		
-		## set the inverse matrix
-		setInverse <- function(newInverseMatrix) inverseMatrix <<- newInverseMatrix
+	## set the inverse matrix
+	setInverse <- function(newInverseMatrix) inverseMatrix <<- newInverseMatrix
 				
-		## get the inverse matrix
-		getInverse <- function() inverseMatrix
+	## get the inverse matrix
+	getInverse <- function() inverseMatrix
 		
-		## calling "makeCacheMatrix" returns a list of functions
+	## calling "makeCacheMatrix" returns a list of functions
     list(set = set, get = get,
          setInverse = setInverse,
          getInverse = getInverse)
@@ -35,25 +35,25 @@ makeCacheMatrix <- function(matrix = matrix()) {
 ## Function parameter is a custom matrix object created by a previous call to makeCacheMatrix()
 cacheSolve <- function(matrix, ...) {
 
-		## for that matrix return the inverse matrix by calling getInverse from makeCacheMatrix()
-		inverse <- matrix$getInverse()
-		
-		## if the inverse has been calculated previously return it from the cache
-		if (!is.null(inverse)) {
-				message("Getting cached data")
-				return(inverse)
-		}
-		
-		## otherwise
-		## get the actual matrix from makeCacheMatrix() ...
-		data <- matrix$get()
-		
-		##  ... calculate the inverse ...
-		inverse <- solve(data)
-		
-		## ... and set the inverse within the special object to retrieve it later thereby eleminating the need to re-calculate it
-		matrix$setInverse(inverse)
+    ## for that matrix return the inverse matrix by calling getInverse from makeCacheMatrix()
+	inverse <- matrix$getInverse()
 
-        ## return a matrix that is the inverse of 'matrix'
-        inverse
+	## if the inverse has been calculated previously return it from the cache
+    if (!is.null(inverse)) {
+	    message("Getting cached data")
+		return(inverse)
+	}
+		
+	## otherwise
+	## get the actual matrix from makeCacheMatrix() ...
+	data <- matrix$get()
+
+	##  ... calculate the inverse ...
+	inverse <- solve(data)
+		
+	## ... and set the inverse within the special object to retrieve it later thereby eleminating the need to re-calculate it
+	matrix$setInverse(inverse)
+
+    ## return a matrix that is the inverse of 'matrix'
+    inverse
 }
